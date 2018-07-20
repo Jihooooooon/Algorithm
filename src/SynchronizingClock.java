33ade0d7392ce2ddcb7599dfe41cab02944e9791
Sombraa <seodosa5@gmail.com>
2018-07-21 01:43:00 +0900

    honeypot

--- src/SynchronizingClock.java
+++ src/SynchronizingClock.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class SynchronizingClock {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String _inputCount = scan.nextLine();
+        int inputCount = Integer.parseInt(_inputCount);
+        int count=0;
+        int finalCount[] = new int[inputCount];
+
+        while(count<inputCount){
+
+        }
+
+    }
 }
